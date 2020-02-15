6b9ff20d26d90aa8d131a05e1c33ad9b9e365cb3
DEATHKNIGHT <ASerFilyakov@mail.ru>
2020-02-15 21:57:19 +0300

    Initial commit

---
+++ desktop/src/ru/phantomhunter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.phantomhunter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.phantomhunter.MyGameGalaxyX;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGameGalaxyX(), config);
+	}
+}
