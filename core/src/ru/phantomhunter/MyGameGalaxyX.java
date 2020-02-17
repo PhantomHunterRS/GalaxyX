4c0f6d10b60fdd71e45d39f6ce79e5e01155dfdd
DEATHKNIGHT <ASerFilyakov@mail.ru>
2020-02-18 01:34:28 +0300

    Lesson1 completion

--- core/src/ru/phantomhunter/MyGameGalaxyX.java
+++ core/src/ru/phantomhunter/MyGameGalaxyX.java
@@ -1,36 +1,15 @@
 package ru.phantomhunter;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class MyGameGalaxyX extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img,img2,img1;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		img1 = new Texture("Space1920x1200.jpg");
-		img2 = new Texture("StarsSky2048x1152.jpg");
-	}
+import ru.phantomhunter.screen.MenuScreen;
+
+
+public class MyGameGalaxyX extends Game {
+
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img1, 0, 0, 650, 600);
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void create() {
+		setScreen(new MenuScreen());
 	}
 }
